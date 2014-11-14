1f6de1368fa52453a384cf0bfcd8a06257993e74
João Pedro Holanda <jpholanda.prf@gmail.com>
2014-11-13 22:51:25 -0300

    Atualizado character.h

--- protótipo gráfico/source/character.h
+++ protótipo gráfico/source/character.h
@@ -7,19 +7,16 @@ class Character : public Entity
 {
 protected:	
 	int _hp;
-	int _vel;
 
 public:
-	Character(): _hp(5), _vel(1) {}
+	Character(): _hp(5) {}
 	
 	void move_to(Position& p) { _pos = p; }
 	void receive_dmg(int dmg) { _hp -= dmg; }
 	
 	int hp() const { return _hp; }
-	int vel() const { return _vel; }
 
 	int& hp() { return _hp; }
-	int& vel() { return _vel; }
 };
 
 
