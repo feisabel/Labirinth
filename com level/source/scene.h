7b57b46b82aba2eaa710fc06971260186d486037
carol_mb <a_caroline96@hotmail.com>
2014-11-24 23:20:00 -0300

    comentários

--- com level/source/scene.h
+++ com level/source/scene.h
@@ -7,16 +7,19 @@
 const int WINDOW_WIDTH = 560;
 const int WINDOW_HEIGHT = 560;
 
+/*
+	Classe abstrata para representar as cenas
+*/
 class Scene
 {
 public:
 	Scene () : b_redraw(true) {}
-    virtual void update() = 0;
+    virtual void update() = 0; //método virtual que é implementado pelas classes filhas
     virtual void redraw() = 0;
 
     static void close();
     
-    bool b_redraw;
+    bool b_redraw; //variável que indica se precisa desenhar novamente na tela 
 
 protected:
     static sf::RenderWindow window;
