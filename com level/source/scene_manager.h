dbb97defc63342e9103a839b79f274def8e39731
carol_mb <a_caroline96@hotmail.com>
2014-11-24 23:22:24 -0300

    comentários

--- com level/source/scene_manager.h
+++ com level/source/scene_manager.h
@@ -3,15 +3,16 @@
 
 #include "scene.h"
 
+//classe que administra as cenas
 class SceneManager
 {
-	static Scene* current_scene;
+	static Scene* current_scene; //ponteiro estático para a cena mostrada na janela
 
 public:
-	static void update() { current_scene->update(); }
-	static void redraw() { current_scene->redraw(); }
+	static void update() { current_scene->update(); } //chama o update da cena atual
+	static void redraw() { current_scene->redraw(); } //chama o redraw da cena atual
 
-	static void change_scene(Scene& s)
+	static void change_scene(Scene& s) //método para trocar a cena atual
 	{
 		current_scene = &s;
 		current_scene->b_redraw = true;
