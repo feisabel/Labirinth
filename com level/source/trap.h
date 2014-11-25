22a04bebdb059b1453299fdf2bc5c719c49df8f0
Leonardo Leibovitz <leibovitz1@gmail.com>
2014-11-25 01:21:48 +0000

    comentários

--- com level/source/trap.h
+++ com level/source/trap.h
@@ -3,17 +3,23 @@
 
 #include "entity.h"
 
+/*
+	classe Trap:
+		Representa uma armadilha. Se o jogador se encontrar no mesmo bloco que ela enquanto ela estiver ativa, ele levará dano.
+		Inimigos não sofrem danos de armadilhas.
+*/
+
 class Trap : public Entity
 {
 private:
-	bool activated;
+	bool activated;  // variável booleana que define se a armadilha está ou não ativada
 
 public:
-	Trap(): activated(false) {}
+	Trap(): activated(false) {} // construtor: inicia a armadilha como estando desativada
 
-	bool is_active() { return activated; }
-	void activate() { activated = true; }
-	void disactivate() { activated = false; }
+	bool is_active() { return activated; }    // método que retorna o estado da armadilha (ativada ou desativada)
+	void activate() { activated = true; }     // ativa a armadilha
+	void disactivate() { activated = false; } // desativa a armadilha
 };
 
 
