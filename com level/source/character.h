8c65cad44cd0b21e3930eb1ad2e5f225afb84b05
feisabel <fernandampi@yahoo.com.br>
2014-11-24 21:16:34 -0400

    character.h comentado

--- com level/source/character.h
+++ com level/source/character.h
@@ -3,22 +3,21 @@
 
 #include "entity.h"
 
+//classe Character representa os personagens do jogo: os monstros e o jogador
+
 class Character : public Entity
 {
 protected:
-	int _hp;
-	int _vel;
+	int _hp; //quantidade de hit points (vidas) que o personagem possui
 
 public:
-	Character(): _hp(5), _vel(0) {}
+	Character(): _hp(5) {} //começa com 5 vidas
 
-	void receive_dmg(int dmg) { _hp -= dmg; if (_hp < 0) _hp = 0; }
+	void receive_dmg(int dmg) { _hp -= dmg; if (_hp < 0) _hp = 0; } //diminui a quantidade de vidas em um certo valor (dmg) recebido por parâmetro
 
-	int hp() const { return _hp; }
-	int vel() const { return _vel; }
+	int hp() const { return _hp; } //retorna a quantidade de vidas
 
-	int& hp() { return _hp; }
-	int& vel() { return _vel; }
+	int& hp() { return _hp; } //retorna a variável da quantidade de vidas
 };
 
 
