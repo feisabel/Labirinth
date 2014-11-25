6708c9c7cb299a65998c6a8a2b50dadb94a0321f
Leonardo Leibovitz <leibovitz1@gmail.com>
2014-11-25 01:02:50 +0000

    comentários

--- com level/source/enemy.h
+++ com level/source/enemy.h
@@ -37,27 +37,27 @@ public:
 	void awake();				 // troca de estado pra AWAKE
 	bool is_awake() const;		 // se o estado é AWAKE
 
-	void init_chase();			
-	bool is_chasing() const;
+	void init_chase();			 // inicia a perseguição do player
+	bool is_chasing() const;     // indica estado de perseguição ou de inatividade
 
-	bool hit_player();
-    bool hit_bullet();
+	bool hit_player();           // indica se o inimigo está atacando o player no loop atual
+    bool hit_bullet();           // indica se o inimigo foi atingido por um tiro no loop atual
 
 	// perseguição
-	bool trace_route(Player&, Maze&);
-	bool chase(Player&, Maze&, queue<Position>&);
+	bool trace_route(Player&, Maze&);             // traça a rota de perseguição a ser tomada pelo inimigo para alcançar o jogador
+	bool chase(Player&, Maze&, queue<Position>&); // a função que faz com que o inimigo persiga o jogador
 
 private:
-	State state;
+	State state; 	  		// define o estado atual do inimigo (SLEEP ou AWAKE)
 
-	queue<Position> route;
+	queue<Position> route;  // define em qual posição da rota (caso esteja em perseguição) o inimigo se encontra
 
-	Direction dir;
+	Direction dir;          // final da rota
 
-	bool chasing;
-	bool hit_p;
-    bool hit_b;
-	bool set_awake;
+	bool chasing;           // define se está ou não em perseguição
+	bool hit_p;             // variável booleana que define se está atacando o player ou não
+    bool hit_b;             // variável booleana que define se levou um tiro ou não
+	bool set_awake;         // define que está AWAKE ou SLEEP
 };
 
 
